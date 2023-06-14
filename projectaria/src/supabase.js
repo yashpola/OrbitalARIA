0748d3f0c41af36443b2100f59b3ef2cf4c9055d
YashyPola <yashwitpo@gmail.com>
2023-06-14 17:24:57 +0800

    v4.0.1: removed dotenv file

--- projectaria/src/supabase.js
+++ projectaria/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const projectURL = process.env.REACT_APP_PROJECT_URL;
-const apiKey = process.env.REACT_APP_API_KEY;
-
-export const supabase = createClient(projectURL, apiKey);
+export const supabase = createClient(
+  "https://rbfpsmkfdaqlxiddervf.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJiZnBzbWtmZGFxbHhpZGRlcnZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODQ2MTI0MzYsImV4cCI6MjAwMDE4ODQzNn0.Bjv8FtEs7nt4I3CRjiEosV6OgggBYcUSujZxDd1NlDM "
+);
